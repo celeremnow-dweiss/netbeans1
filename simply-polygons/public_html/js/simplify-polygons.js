7eead0c636e16fd752412677ef9bc1820d128831
Daniel Weiss <dan@celeremnow.com>
2018-02-05 17:38:29 -0800

    This is my first commit test

--- simply-polygons/public_html/js/simplify-polygons.js
+++ simply-polygons/public_html/js/simplify-polygons.js
@@ -4,35 +4,34 @@
  * and open the template in the editor.
  */
 
-function test()
-{
-
-
-var x = 5;
-var y = 6;
-
-alert(x*y);
-};
-
-function DissolveTwoGeomsWithJSTS(wkt1, wkt2)
-{
-    // Instantiate JSTS WKTReader and get two JSTS geometry objects
-    var wktReader = new jsts.io.WKTReader();
-    var geom1 = wktReader.read(wkt1);
-    var geom2 = wktReader.read(wkt2);
-
-    // In JSTS, "union" is synonymous with "dissolve"
-    var dissolvedGeometry = geom1.union(geom2);
-
-    /* Since 'union()' is a method of a JSTS geometry object, you
-       could easily modify this method to iterate over an array
-       of JSTS geometry objects, calling a 'union()' on each 
-       sequential object. */
-
-    // Instantiate JSTS WKTWriter and get new geometry's WKT
-    var wktWriter = new jsts.io.WKTWriter();
-    var wkt = wktWriter.write(dissolvedGeometry);
-
-    return wkt;
-    alert(wkt);
+function test() {
+    // find the element in the DOM
+    var box1 = document.getElementById("v1");
+    var box2 = document.getElementById("v2");
+    
+    if box1.value 
+        {if box2.value
+            {
+            // This creates a new element.  
+            var newElement = document.createElement("div");
+            // Set the value to what you want
+            newElement.innerHTML = box1.value * box2.value
+            document.body.appendChild(newElement);
+            }
+        else
+            {
+                alert('You haven\'t filled in ' + box2.name + '!');
+                box2.focus()
+                // No returns necessary, since we're not dealing with formsubmittal.
+            }
+    else
+        { 
+            alert('You haven\'t filled in ' + box1.name + '!');
+            box1.focus()
+        }
+    newElement.innerHTML = box.value;
+    document.body.appendChild(newElement)
 }
+
+// This hooks up the function we just wrote to the click event of the button.
+document.getElementById("button1").onclick = test;
